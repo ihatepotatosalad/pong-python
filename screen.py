c49f52e3eaed6356c7214c13ee90777666953d2d
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-12-28 19:21:26 -0500

    finishing touches

--- screen.py
+++ screen.py
@@ -7,21 +7,7 @@ class Screen_Start(Turtle):
         self.points_player1 = 0
         self.points_bot = 0
         self.players = []
-        player_score = Turtle()
-        player_score.color('white')
-        player_score.penup()
-        player_score.goto(-300, 300)
-        player_score.hideturtle()
-        
-        player_score.write(f'player1: {self.points_player1}')
-        bot_score = Turtle()
-        bot_score.goto(300, 300)
-        bot_score.hideturtle()
-        bot_score.color('white')
-        bot_score.penup()
-        bot_score.write(f'player2: {self.points_bot}')
-        self.players.append(player_score)
-        self.players.append(bot_score)
+        self.score_board()
 
     def middle_line(self):
         middle_line = Turtle()
@@ -31,6 +17,23 @@ class Screen_Start(Turtle):
         middle_line.setposition(0, -400)
         middle_line.pendown()
         middle_line.setposition(0, 400)
+
+    def score_board(self):
+        player_score = Turtle()
+        player_score.color('white')
+        player_score.penup()
+        player_score.goto(-300, 300)
+        player_score.hideturtle()
+
+        player_score.write(f'player1: {self.points_player1}')
+        bot_score = Turtle()
+        bot_score.goto(300, 300)
+        bot_score.hideturtle()
+        bot_score.color('white')
+        bot_score.penup()
+        bot_score.write(f'player2: {self.points_bot}')
+        self.players.append(player_score)
+        self.players.append(bot_score)
 
     def points_player(self):
         self.points_player1 += 1
